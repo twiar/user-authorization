20ce48196e6642500e43f28509e815c35bfc1c81
zhitenev.andr <zhitenev.andr@gmail.com>
2023-02-16 17:27:49 +0300

    quazar setup & auth page

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="modal flex flex-center column">
+      <h2 class="text-h2">Авторизация</h2>
+      <q-input outlined v-model="text" placeholder="Логин" />
+      <q-input outlined v-model="password" type="password" placeholder="Пароль" />
+      <q-btn color="primary" label="Отправить запрос" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+});
+</script>
